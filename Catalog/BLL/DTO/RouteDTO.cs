f55f905d4c39a70dd6e206331319253682c21cb6
Serhiy Kondratenko <seriy.cub@gmail.com>
2024-12-12 16:31:08 +0200

    Added DTO objects

---
+++ Catalog/BLL/DTO/RouteDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TransportEnterprise.Catalog.BLL.DTO;
+
+public class RouteDTO
+{
+    public int RouteId { get; set; }
+    public string StartingPoint { get; set; }
+    public string EndPoint { get; set; }
+    public float Length { get; set; }
+    public int TransportEnterpriseId { get; set; }
+}
---
+++ Catalog/BLL/DTO/VehicleDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportEnterprise.Catalog.BLL.DTO;
+
+public class VehicleDTO
+{
+    public int VehicleId { get; set; }
+    public string LicensePlate { get; set; }
+    public string Model { get; set; }
+    public int ProductionYear { get; set; }
+    public string Type { get; set; }
+    public int TransportEnterpriseId { get; set; }
+}
